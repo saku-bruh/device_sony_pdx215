3358d4dbce919e370bf73e056dee12e38faec31c
Saku <67262050+saku-bruh@users.noreply.github.com>
2022-09-26 23:19:55 +0300

    pdx215: fix kernel path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,7 +120,7 @@ BOARD_KERNEL_CMDLINE += pcie_ports=compat
 BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
-TARGET_KERNEL_SOURCE := kernel/sony/pdx215
+TARGET_KERNEL_SOURCE := kernel/sony/sm8350
 TARGET_KERNEL_CONFIG := vendor/pdx215_defconfig
 TARGET_KERNEL_CLANG_VERSION := prelude
 KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-prelude/bin
