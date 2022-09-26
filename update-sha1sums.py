a2bcb1b2d9778f459bbfca02d548b779471de564
Saku <67262050+saku-bruh@users.noreply.github.com>
2022-09-26 23:57:33 +0300

    Update update-sha1sums.py

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -20,8 +20,8 @@ import os
 import sys
 from hashlib import sha1
 
-device = 'sm8350-common'
-vendor = 'xiaomi'
+device = 'pdx215'
+vendor = 'sony'
 
 with open('proprietary-files.txt', 'r') as f:
     lines = f.read().splitlines()
